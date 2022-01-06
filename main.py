70cba14e7e240fe459990dff2f68875684c0ebbe
Macarena Acosta <macarenaa988@gmail.com>
2022-01-06 20:38:42 -0300

    main file

---
+++ main.py
@@ -0,0 +1,20 @@
+from myServerPage import MyServerPage
+from locators import Locators
+
+driver_my_server = MyServerPage()
+
+driver_my_server.get_web_page("https://myserver.gg/es")
+
+search_bar = driver_my_server.get_element(Locators.search_bar)
+
+driver_my_server.click(search_bar)
+
+driver_my_server.send_keys(search_bar, "music")
+
+link = driver_my_server.get_element(Locators.link)
+
+driver_my_server.click(link)
+
+driver_my_server.back()
+
+driver_my_server.quit()
