88bcf150d3af40a913a987ba42f424da14192f11
Macarena Acosta <macarenaa988@gmail.com>
2022-01-06 22:13:23 -0300

    adding new functions

--- main.py
+++ main.py
@@ -5,15 +5,9 @@ driver_my_server = MyServerPage("your path")
 
 driver_my_server.get_web_page("https://myserver.gg/es")
 
-search_bar = driver_my_server.get_element(Locators.search_bar)
+driver_my_server.click_and_send_keys(Locators.search_bar, "music")
 
-driver_my_server.click(search_bar)
-
-driver_my_server.send_keys(search_bar, "music")
-
-link = driver_my_server.get_element(Locators.link)
-
-driver_my_server.click(link)
+driver_my_server.click(Locators.link)
 
 driver_my_server.back()
 
