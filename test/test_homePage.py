da33eb4df9eb6716376d87ef7c50e84dab59b1b2
Macarena Acosta <macarenaa988@gmail.com>
2023-08-21 20:45:04 -0300

    added test

---
+++ test/test_homePage.py
@@ -0,0 +1,14 @@
+import pytest
+from main.pages.home_page import HomePage
+
+
+@pytest.mark.usefixtures("setup")
+class TestHomePage:
+
+    def test_search_jigsaw_by_category(self):
+        category = "simpsons"
+        home_page = HomePage(self.driver)
+        jigsaw_list_page = home_page.search_jigsaw(category)
+        assert len(jigsaw_list_page.get_pages_links()) == 5
+        assert "9098" in jigsaw_list_page.get_results()
+        assert jigsaw_list_page.get_category() == category
