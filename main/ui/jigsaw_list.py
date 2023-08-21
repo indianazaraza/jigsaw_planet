7192b2769b89f5956119fbbc2119a4141cd6869f
Macarena Acosta <macarenaa988@gmail.com>
2023-08-21 20:45:39 -0300

    class that contains all locators from jigsaw list page

---
+++ main/ui/jigsaw_list.py
@@ -0,0 +1,6 @@
+
+
+class JigsawList:
+    results = "//div[@style='margin:var(--vertical-spacing) 0;display:flex;flex-wrap:wrap;align-items:center']/div[1]"
+    pages_list = "//ul[@class='paginator unselectable']//a[@href]"
+    category = "//span[@style='display:inline-flex;align-items:center;font-weight:bold']"
